b4d8f64c7126c53cacbbed663717da8c2f4377f2
BryanGutierrez <bgr_96@hotmail.com>
2018-05-25 01:03:07 -0500

    first commit

---
+++ app/src/main/java/com/example/bryan/calllogprovider/Call.java
@@ -0,0 +1,45 @@
+package com.example.bryan.calllogprovider;
+
+
+public class Call {
+    String number,name,duration,type;
+
+    public Call(String number, String name, String duration, String type) {
+        this.number = number;
+        this.name = name;
+        this.duration = duration;
+        this.type = type;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
